53aa75335597936d5758d2bf4219d09d6cab47bd
Ali MAksoud <ali.elmaksoud@gmail.com>
2020-10-01 09:38:31 +0300

    corrected-completed step 5

--- javascript-basics-02/step 5/step_05.js
+++ javascript-basics-02/step 5/step_05.js
@@ -1,13 +1,24 @@
-var img = document.querySelector("#image1");
-var img2 = document.querySelector("#image2");
-var img3 = document.querySelector("#image3");
-var img4 = document.querySelector("#image4");
-var img5 = document.querySelector("#image5");
+    function a(){
 
-img.addEventListener("mouseover" , function(){
-    img.src = "images/image1_2.jpg";
-    img2.src = "images/image2_2.jpg";
-    img3.src = "images/image3_2.jpg";
-    img4.src = "images/image4_2.jpg";
-    img5.src = "images/image5_2.jpg";
-})
+    var image1 = document.querySelector("#image1")
+    var image2 = document.querySelector("#image2")
+    var image3 = document.querySelector("#image3")
+    var image4 = document.querySelector("#image4")
+    var image5 = document.querySelector("#image5")
+    image1.addEventListener('mouseover', function (){
+    image1.src = "images/image1_2.jpg"
+    })
+    image2.addEventListener('mouseover' , function (){
+       image2.src= "images/image2_2.jpg"
+    })
+    image3.addEventListener('mouseover', function (){
+       image3.src = "images/image3_2.jpg"
+    })
+    image4.addEventListener('mouseover', function (){
+        image4.src = "images/image4_2.jpg"
+    })
+    image5.addEventListener('mouseover', function (){
+        image5.src = "images/image5_2.jpg"
+    })
+    
+    }
